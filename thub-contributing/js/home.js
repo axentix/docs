cc8776faf37e5472f068af3a185120a8bf79ea2e
Stallos11 <49057921+Stallos11@users.noreply.github.com>
2021-08-28 09:40:00 +0000

    Deploying to gh-pages from @ axentix/docs@2119da11ac5f0ca036c93fbcbd6d01eeb77f11f9 🚀

---
+++ thub-contributing/js/home.js
@@ -0,0 +1,71 @@
+var sidenav = new Axentix.Sidenav('#main-sidenav');
+var collapsible = new Axentix('collapsible');
+
+const HomeAxentix = (() => {
+  let hiddenSentence,
+    sentenceCheckbox,
+    colors,
+    btnColors,
+    currentColor = 'blue',
+    neuSwitch,
+    neuElems,
+    neuTheme = 'dark';
+
+  const updateSentence = () => {
+    sentenceCheckbox.checked ? hiddenSentence.classList.add('visible') : hiddenSentence.classList.remove('visible');
+  };
+
+  /** Colors */
+  const updateColors = (e) => {
+    const selectedColor = e ? e.target.dataset.colorBtn : currentColor;
+
+    colors.map((color) => {
+      const type = color.dataset.colors.split(',');
+      type.map((colType) => {
+        if (colType === 'txt' || colType === 'bd') {
+          color.classList.remove(`${colType}-${currentColor}`);
+          color.classList.add(`${colType}-${selectedColor}`);
+        } else if (colType === 'bg') {
+          color.classList.remove(currentColor);
+          color.classList.add(selectedColor);
+        }
+      });
+    });
+
+    currentColor = selectedColor;
+  };
+
+  /** Neu */
+  const switchNeuTheme = () => {
+    neuElems.map((elem) => {
+      elem.classList.remove(`neu-${neuTheme}`);
+      neuTheme = neuTheme === 'light' ? 'dark' : 'light';
+      elem.classList.add(`neu-${neuTheme}`);
+    });
+  };
+
+  const setup = () => {
+    hiddenSentence = document.querySelector('#hidden-sentence');
+    sentenceCheckbox = document.querySelector('#sentence-checkbox');
+
+    sentenceCheckbox.addEventListener('change', updateSentence);
+
+    colors = Array.from(document.querySelectorAll('[data-colors]'));
+    btnColors = Array.from(document.querySelectorAll('[data-color-btn]'));
+
+    updateColors();
+
+    btnColors.map((btn) => btn.addEventListener('click', updateColors));
+
+    neuSwitch = document.querySelector('#neu-switch');
+    neuSwitch.addEventListener('change', switchNeuTheme);
+
+    neuElems = Array.from(document.querySelectorAll('[data-neu]'));
+  };
+
+  return {
+    setup,
+  };
+})();
+
+document.addEventListener('DOMContentLoaded', HomeAxentix.setup);
